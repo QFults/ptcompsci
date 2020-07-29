b3d339a916405c35c2714eb54c0cb27c53801623
Quinton Fults <quinton.fults.work@gmail.com>
2020-07-28 19:05:57 -0700

    code

--- index.js
+++ index.js
@@ -2,9 +2,22 @@ const stuff = [10, 34, 56, 67, 93, 120, 137, 168, 259, 280, 311, 342, 413, 514]
 
 const random = stuff[Math.floor(Math.random() * stuff.length)]
 
-for (let i = 0; i < stuff.length; i++) {
-  if (stuff[i] === random) {
-    console.log(stuff[i], random, i)
+const linearSearch = () => { 
+  for (let i = 0; i < stuff.length; i++) {
+    if (stuff[i] === random) {
+      return i
+    }
   }
 }
 
+const { Suite, filter } = require('benchmark')
+
+const suite = new Suite()
+
+suite
+  .add('Linear Search', linearSearch)
+  .on('complete', function () {
+    const benchmark = filter(this, 'successful')[0]
+    console.log(benchmark.name, benchmark.stats.mean)
+  })
+  .run()
